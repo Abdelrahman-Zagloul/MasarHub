28727b71db657c10cd8150a3c9a097f24969ef7a
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2026-06-14 23:50:07 +0300

    test(verify-authenticator): add unit test for verify authenticator handler and validator

---
+++ tests/Unit/MasarHub.Application.UnitTests/Features/Authentication/Commands/TwoFactor/VerifyAuthenticator/VerifyAuthenticatorCommandValidatorTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using MasarHub.Application.Features.Authentication.Commands.TwoFactor.VerifyAuthenticator;
+
+namespace MasarHub.Application.UnitTests.Features.Authentication.Commands.TwoFactor.VerifyAuthenticator
+{
+    [Trait("UnitTests", "Feature.Auth.VerifyAuthenticator.Validator")]
+    public sealed class VerifyAuthenticatorCommandValidatorTests
+    {
+        private readonly VerifyAuthenticatorCommandValidator _sut = new();
+
+        [Fact]
+        public void Validate_ValidCode_ReturnsNoErrors()
+        {
+            var command = new VerifyAuthenticatorCommand("123456");
+
+            var result = _sut.Validate(command);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("12")]
+        [InlineData("abcdef")]
+        public void Validate_InvalidCode_ReturnsValidationError(string code)
+        {
+            var command = new VerifyAuthenticatorCommand(code);
+
+            var result = _sut.Validate(command);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == "Code");
+        }
+    }
+}
