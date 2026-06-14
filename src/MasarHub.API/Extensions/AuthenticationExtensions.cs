7a7c6ae6a2ba51e18bc18852d1266b9305360b43
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2026-06-15 01:11:18 +0300

    feat(api): add custom JWT challenge and forbidden responses

--- src/MasarHub.API/Extensions/AuthenticationExtensions.cs
+++ src/MasarHub.API/Extensions/AuthenticationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using MasarHub.Application.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -29,6 +30,48 @@ public static class AuthenticationExtensions
                     ValidAudience = jwtSettings.Audience,
 
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnChallenge = async context =>
+                    {
+                        context.HandleResponse();
+
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        context.Response.ContentType = "application/problem+json";
+                        await context.Response.WriteAsJsonAsync(new ProblemDetails
+                        {
+                            Type = "https://httpstatuses.com/401",
+                            Title = "Unauthorized",
+                            Detail = "Authentication is required.",
+                            Status = StatusCodes.Status401Unauthorized,
+                            Instance = context.Request.Path,
+                            Extensions =
+                            {
+                                ["traceId"] = context.HttpContext.TraceIdentifier
+                            }
+                        });
+                    },
+
+                    OnForbidden = async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                        context.Response.ContentType = "application/problem+json";
+
+                        await context.Response.WriteAsJsonAsync(new ProblemDetails
+                        {
+                            Type = "https://httpstatuses.com/403",
+                            Title = "Forbidden",
+                            Detail = "You do not have permission to access this resource.",
+                            Status = StatusCodes.Status403Forbidden,
+                            Instance = context.Request.Path,
+                            Extensions =
+                            {
+                                ["traceId"] = context.HttpContext.TraceIdentifier
+                            }
+                        });
+                    }
                 };
             });
 
