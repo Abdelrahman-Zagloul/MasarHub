12629e4604a13e127f5fc634a1ef3c5ccdbb2be3
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2026-06-14 23:50:13 +0300

    test(verify-recovery-code): add unit test for verify recovery code handler and validator

---
+++ tests/Unit/MasarHub.Application.UnitTests/Features/Authentication/Commands/TwoFactor/VerifyRecoveryCode/VerifyRecoveryCodeCommandValidatorTests.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using MasarHub.Application.Features.Authentication.Commands.TwoFactor.VerifyRecoveryCode;
+
+namespace MasarHub.Application.UnitTests.Features.Authentication.Commands.TwoFactor.VerifyRecoveryCode
+{
+    [Trait("UnitTests", "Feature.Auth.VerifyRecoveryCode.Validator")]
+    public sealed class VerifyRecoveryCodeCommandValidatorTests
+    {
+        private readonly VerifyRecoveryCodeCommandValidator _sut = new();
+
+        [Fact]
+        public void Validate_ValidCommand_ReturnsNoErrors()
+        {
+            var command = new VerifyRecoveryCodeCommand(Guid.NewGuid(), "recovery-code-1");
+
+            var result = _sut.Validate(command);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validate_InvalidChallengeId_ReturnsValidationError()
+        {
+            var command = new VerifyRecoveryCodeCommand(Guid.Empty, "recovery-code-1");
+
+            var result = _sut.Validate(command);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == "ChallengeId");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Validate_InvalidCode_ReturnsValidationError(string code)
+        {
+            var command = new VerifyRecoveryCodeCommand(Guid.NewGuid(), code);
+
+            var result = _sut.Validate(command);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == "Code");
+        }
+    }
+}
